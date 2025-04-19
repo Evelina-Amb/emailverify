7a1ac29536bc266e6cd4cbc92c5cf563e84e4ec0
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-04-19 04:53:38 -0700

    email notifikacijos kai updatinama arba pridedama new info

---
+++ Mail/NewStudentNotification.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Student;
+
+class NewStudentNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $student;
+    public function __construct(Student $student)
+    {
+        $this->student = $student;
+    }
+
+    public function build()
+    {
+        return $this->subject('Studento duomenys atnaujinti')
+                    ->view('emails.updated_student');
+    }
+}
